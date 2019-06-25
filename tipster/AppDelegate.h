e9a39847983ed0871ab96be13c0d44672095a541
Julia Pearl <jpearl@fb.com>
2019-06-25 14:44:46 -0700

    Initial Commit

---
+++ tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipster
+//
+//  Created by jpearl on 6/25/19.
+//  Copyright © 2019 jpearl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipster
+//
+//  Created by jpearl on 6/25/19.
+//  Copyright © 2019 jpearl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
